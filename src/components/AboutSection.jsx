4c039c034a3433804ed9b7034aae40b1944e7582
obrucheoghene <wilfredcloudspace@gmail.com>
2025-06-12 00:56:30 +0100

    feat: update cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -57,7 +57,7 @@ export const AboutSection = () => {
 
               <a
                 target="_blank"
-                href="https://drive.google.com/file/d/1ksVgDqoVJtIYAmPbgfVQomoQT3R4-Frz/view?usp=sharing"
+                href="https://drive.google.com/file/d/1EkMkIpJDSRYQFwHSFWThGfwgo2MgveyM/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 onClick={handleLinkClick}
               >
